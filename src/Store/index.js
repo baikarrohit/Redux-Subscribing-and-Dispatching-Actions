fa6ab92b358513ff15489cb531fbabe57905f602
baikarrohit <baikarrohit1999@gmail.com>
2023-09-12 22:09:00 +0530

    Subscribing and Dispatching Actions

---
+++ src/Store/index.js
@@ -0,0 +1,25 @@
+import { createStore } from "redux";
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  } else if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  } else if (action.type === "incrementBy5") {
+    return {
+      counter: state.counter + 5,
+    };
+  } else if (action.type === "decrementBy5") {
+    return {
+      counter: state.counter - 5,
+    };
+  }
+  return state;
+};
+const store = createStore(counterReducer);
+
+export default store;
